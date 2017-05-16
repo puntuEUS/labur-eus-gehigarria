9bf9092ac91c3a44e246393b8997cb1d9c4e3a95
Asier Iturralde Sarasola <asier.iturralde@gmail.com>
2017-05-16 18:00:56 +0200

    Twitter-en botoia gehitzeko lehen proba

---
+++ src/twitter.js
@@ -0,0 +1,11 @@
+$(".TweetBoxExtras.tweet-box-extras").append('<span class="TweetBoxExtras-item"><div class="laburtu">' +
+  '<button class="btn icon-btn laburtu-btn js-tooltip" type="button" data-delay="150" tabindex="0" data-original-title="Laburtu estekak">' +
+    '<span class="laburtu-icon Icon Icon--laburtu"></span>' +
+    '<span class="text laburtu-label u-hiddenVisually">Laburtu</span>' +
+  '</button>' +
+'</div>' +
+'</span>');
+
+$(".laburtu-btn").click(function() {
+    alert("kaixo");
+});
