f6c14bd9d43b55f7bbc3d4b8bd687cbaf7983dfd
Asier Iturralde Sarasola <asier.iturralde@gmail.com>
2017-05-17 10:26:46 +0200

    Botoia behin bakarrik gehitu

--- src/twitter.js
+++ src/twitter.js
@@ -5,13 +5,16 @@ function getArrayOfUrls(text) {
     return text.match(urlRegex);
 }
 
-$(".TweetBoxExtras.tweet-box-extras").append('<span class="TweetBoxExtras-item"><div class="laburtu">' +
-  '<button class="btn icon-btn laburtu-btn js-tooltip" type="button" data-delay="150" tabindex="0" data-original-title="Laburtu estekak">' +
-    '<span class="laburtu-icon Icon Icon--laburtu"></span>' +
-    '<span class="text laburtu-label u-hiddenVisually">Laburtu</span>' +
-  '</button>' +
-'</div>' +
-'</span>');
+if ($(".laburtu-btn").length === 0) {
+
+    $(".TweetBoxExtras.tweet-box-extras").append('<span class="TweetBoxExtras-item"><div class="laburtu">' +
+      '<button class="btn icon-btn laburtu-btn js-tooltip" type="button" data-delay="150" tabindex="0" data-original-title="Laburtu estekak">' +
+        '<span class="laburtu-icon Icon Icon--laburtu"></span>' +
+        '<span class="text laburtu-label u-hiddenVisually">Laburtu</span>' +
+      '</button>' +
+    '</div>' +
+    '</span>');
+}
 
 $(".laburtu-btn").click(function() {
     var tweet = $("#tweet-box-global").text();
