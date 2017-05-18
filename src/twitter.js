11d9bf063e787e0f4c8f1f0071258e3641ec600f
Asier Iturralde Sarasola <asier.iturralde@gmail.com>
2017-05-18 12:33:49 +0200

    Dinamikoki gehitutako tweet-boxak detektatzea lortu. MutationObserver ez dabil. CSS3ko hack bat bai

--- src/twitter.js
+++ src/twitter.js
@@ -54,3 +54,58 @@ document.addEventListener("click", function(e) {
         }
     }
 });
+
+// select the target node
+var target = document.getElementById("permalink-overlay");
+console.log(target);
+// create an observer instance
+var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+        console.log(mutation);
+
+        for (var i = 0; i < mutation.addedNodes.length; i++){
+            console.log(mutation.addedNodes[i]);
+        }
+        var tweet_boxes = target.getElementsByClassName("TweetBoxExtras tweet-box-extras");
+        console.log(tweet_boxes);
+        for (var i = 0; i < tweet_boxes.length; i++) {
+
+            if (tweet_boxes[i].getElementsByClassName("laburtu-btn").length === 0) {
+
+                tweet_boxes[i].innerHTML = tweet_boxes[i].innerHTML + laburtu_button_html;
+                console.log(tweet_boxes[i]);
+
+            }
+        }
+    });
+});
+
+// configuration of the observer:
+var config = {
+    attributes: true,
+    childList: true,
+    characterData: true,
+    subTree: true
+};
+
+// pass in the target node, as well as the observer options
+observer.observe(target, config);
+
+var insertListener = function(event){
+	if (event.animationName == "nodeInserted") {
+		// This is the debug for knowing our listener worked!
+		// event.target is the new node!
+		console.warn("Another node has been inserted! ", event, event.target);
+        console.log(event.target);
+        if (event.target.getElementsByClassName("laburtu-btn").length === 0) {
+
+            event.target.innerHTML = event.target.innerHTML + laburtu_button_html;
+            console.log(event.target);
+
+        }
+	}
+}
+
+addEventListener("animationstart", insertListener, false); // standard + firefox
+document.addEventListener("MSAnimationStart", insertListener, false); // IE
+document.addEventListener("webkitAnimationStart", insertListener, false); // Chrome + Safari
